0ac72c58006db50e92b151d8e02703101b216957
mansmahamat <mansour.mahamat.salle@gmail.com>
2021-07-28 17:02:23 +0200

    :art: footer
    footer

--- Desktop/Perso/shop/frontend/src/App.js
+++ Desktop/Perso/shop/frontend/src/App.js
@@ -7,7 +7,12 @@ function App() {
     <>
       <Header />
       <main className="container mx-auto py-4">
-        <h1 className="text-green-400 text-2xl">Welcome to PlantShop </h1>
+        <h1
+          className="text-green-400 text-2xl
+        "
+        >
+          Welcome to PlantShop{' '}
+        </h1>
       </main>
       <Footer />
     </>
