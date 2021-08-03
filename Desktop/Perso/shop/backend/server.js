856b5fc8fc5faf23d71723d2dd315633dc94935b
mansmahamat <mansour.mahamat.salle@gmail.com>
2021-08-03 16:04:01 +0200

    :art: Add Redux
    Add Redux

--- Desktop/Perso/shop/backend/server.js
+++ Desktop/Perso/shop/backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const products = require('./data/products');
 const connectDb = require('./config/db');
+
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 
@@ -13,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/plants', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/plants/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/plants', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
